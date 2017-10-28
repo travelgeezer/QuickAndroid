89fb6ebac575b8c368492225cff84663140f6606
travelgeezer <travelgeezer@icloud.com>
2017-10-28 11:55:53 +0800

    [T] feature: add java8 language features

--- app/src/main/java/com/example/geezer/quickandroid/MainActivity.java
+++ app/src/main/java/com/example/geezer/quickandroid/MainActivity.java
@@ -1,7 +1,8 @@
 package com.example.geezer.quickandroid;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // use lambda expressions
+        findViewById(R.id.button).setOnClickListener(v ->
+                Toast.makeText(this, "click button", Toast.LENGTH_SHORT).show());
     }
+
 }

2e0fa02d8b28198b94c45cf30d1fa99069f22435
travelgeezer <travelgeezer@icloud.com>
2017-10-28 20:31:01 +0800

    [T] app: create sample application

---
+++ app/src/main/java/com/example/geezer/quickandroid/managers/app/SampleApplication.java
@@ -0,0 +1,18 @@
+package com.example.geezer.quickandroid.managers.app;
+
+import android.app.Application;
+
+import com.example.geezer.quickandroid.utils.Utils;
+
+
+/**
+ * Created by geezer. on 28/10/2017.
+ */
+
+public class SampleApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
